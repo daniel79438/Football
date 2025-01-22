21e51b0696698683cee29a9d5c71b6dd9cb3c57d
Daniel Harris <daniel79438@gmail.com>
2025-01-22 20:53:15 +0000

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Daniel Harris on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
